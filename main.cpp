a6db4194a9bbef126455161169fa0fd70b804056
niheper <159460032+tethranialn@users.noreply.github.com>
2024-12-06 21:31:29 +0300

    проверка числа точек и файлов

--- main.cpp
+++ main.cpp
@@ -6,15 +6,22 @@
 using namespace std;
 const unsigned N = 100;
 
-int readFile(fstream& inputFile, float x[N], float y[N]);
+int readFile(fstream& inputFile, float x[N], float y[N], int& big_number);
 void main(void)
 {
 	fstream inputFile;
-	float x[N], y[N]; char tryAgain = '!'; unsigned file; bool fileSelected = false;
+	float x[N], y[N]; char tryAgain = '!'; unsigned file; bool fileSelected = false; int big_number = 0;
 	//file selection
 	while (fileSelected != true)
 	{
-		cout << skipws << "available files:\n\t0. \"in.txt\"\n\t1. \"in1.txt\"\n\t2. \"in2.txt\"\nselect the file to open: ";
+		cout << skipws << "available files:\n";
+		cout << "\t0. \"in.txt\"\n";
+		cout << "\t1. \"in1.txt\"\n";
+		cout << "\t2. \"in2.txt\"\n";
+		cout << "\t3. \"in3.txt\"\n";
+		cout << "\t4. \"in4.txt\"\n";
+		cout << "\t5. \"in5.txt\"\n";
+		cout<<"select the file to open : ";
 		cin >> file;
 		switch (file)
 		{
@@ -36,9 +43,27 @@ void main(void)
 			fileSelected = true;
 			break;
 		}
+		case 3:
+		{
+			inputFile.open("in3.txt", ios::in);
+			fileSelected = true;
+			break;
+		}
+		case 4:
+		{
+			inputFile.open("in4.txt", ios::in);
+			fileSelected = true;
+			break;
+		}
+		case 5: 
+		{
+			inputFile.open("in5.txt", ios::in);
+			fileSelected = true;
+			break;
+		}
 		default:
 		{
-			cout << "\nerror: file is not exist. please, press enter to try again: " << noskipws;
+			cout << "\nerror: this file is not exist. please, press enter to try again: " << noskipws;
 			cin.ignore();
 			cin.get();
 			system("cls");
@@ -47,31 +72,52 @@ void main(void)
 		}
 		}
 	}
-	switch (readFile(inputFile, x, y))
+	//checking file opening
+	if (!inputFile.is_open())
 	{
-	case -1:
-	{
-		cout << "\nthe number of points cannot be less than zero";
+		cout << "this file is not open or does not exist. please, restart the programm";
 		return;
 	}
+	//checking number of points 
+	switch (readFile(inputFile, x, y, big_number))
+	{
 	case 0:
 	{
-		cout << "\nsince the number of points is zero, programm cannot be executed";
+		cout << "since the number of points is zero, programm cannot be executed";
 		return;
 	}
-	default:
+	case -1:
 	{
-		cout << "\nfile reading error";
+		cout << "the number of points cannot be less than zero";
+		return;
+	}
+	case -2:
+	{
+		cout << "file is empty";
+		return;
+	}
+	case -3:
+	{
+		cout << "file reading error";
 		return;
 	}
 	}
+	if (big_number != 0)
+	{
+		cout << "specified number of points (" << big_number << ") has been reduced to " << N << '\n';
+	}
+	cout << "Hello world!";
 	inputFile.close();
 }
-int readFile(fstream& inputFile, float x[N], float y[N])
+int readFile(fstream& inputFile, float x[N], float y[N], int& big_number)
 {
 	int number;
 	inputFile >> number;
-	if (number < 0)
+	if (inputFile.eof())
+	{
+		return -2;
+	}
+	else if (number < 0)
 	{
 		return -1;
 	}
@@ -81,11 +127,13 @@ int readFile(fstream& inputFile, float x[N], float y[N])
 	}
 	else if (number > N)
 	{
+		big_number = number;
+		number = N;
 		return number;
 	}
 	else
 	{
 		return number;
 	}
-
+	return -3;
 }
